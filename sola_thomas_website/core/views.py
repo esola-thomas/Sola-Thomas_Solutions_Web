efea7f50d9d83cf21475096ce6444f2b6e45e838
Ernesto Sola-Thomas <ernesto@solathomas.com>
2025-02-16 23:31:36 -0500

    Develop the Website Pages and Navigation Structure

---
+++ sola_thomas_website/core/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import ContactForm
+
+def home(request):
+    return render(request, 'core/home.html', {
+        'services': Service.objects.all()[:3],
+        'testimonials': Testimonial.objects.filter(is_featured=True)[:3]
+    })
+
+def about(request):
+    return render(request, 'core/about.html')
+
+def contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Thank you for your message. We will contact you soon!')
+            return redirect('core:contact')
+    else:
+        form = ContactForm()
+    return render(request, 'core/contact.html', {'form': form})
+
+@login_required
+def dashboard(request):
+    return render(request, 'core/dashboard.html', {
+        'bookings': request.user.bookings.all(),
+        'invoices': request.user.invoices.all()
+    })
