f6a70f322dde47c2276432a52f7f45b743ffba82
Ernesto Sola-Thomas <esolathomas@microosft.com>
2025-02-17 23:57:43 -0500

    Add django deploy pipeline to server

--- sola_thomas_website/sola_thomas_website/urls.py
+++ sola_thomas_website/sola_thomas_website/urls.py
@@ -18,10 +18,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
+
+def health_check(request):
+    return HttpResponse("OK", status=200)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
     path('services/', include('services.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('health/', health_check, name='health_check'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
