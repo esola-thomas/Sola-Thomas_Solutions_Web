16e940ce05c4c513b1bbad1f89a17954c7b2def8
Ernesto Sola-Thomas <esolathomas@microosft.com>
2025-02-17 23:32:13 -0500

    Update db file path for non DEBUG

--- sola_thomas_website/sola_thomas_website/settings.py
+++ sola_thomas_website/sola_thomas_website/settings.py
@@ -84,12 +84,20 @@ WSGI_APPLICATION = 'sola_thomas_website.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': Path.home() / 'out' / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
